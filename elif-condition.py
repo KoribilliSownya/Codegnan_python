a7f05bd88ad2c7f3ebf0a5e3fd8e70c6c5c23613
KoribilliSownya <ksownya2002@gmail.com>
2026-05-02 10:28:56 +0530

    Add files via upload
    practice problems on for loops

--- elif-condition.py
+++ elif-condition.py
@@ -68,14 +68,94 @@ if empty == so:
     print("Palindrome")
 else:
     print("Not a palindrome")
-"""
+
 any="koribilli"
 empty=""
 for i in range(len(any)-1,-1,-1):
     empty+=any[i]
 print(empty)
 
+----while statement
+-------->This statement will keep on executing until unless condition becomes true
 
+v=1
+while v <=5:
+    print(v)
+    v+=1
+
+range()---->This range() will generate sequence numbers upto the limit
+syntax----> range(starting,ending,step) step is optional
+
+choice_u=int(input("Enter the limit: "))
+for j in range(100,choice_u+1,2): # j here as a instance variable or temporary variable
+    print(j)
+
+
+for i in range(2,50+1):
+    if i % 2==0:
+        print(f"{i} is even number")
+    else:
+        print(f"{i} is odd number")
+
+break
+------>This break statement will execute if the condition becomes true and never enters into next loops
+
+
+any=["pavani","mahima","sownya","indhrani"]
+for i in any:
+    print(i)
+    if i=="sownya":
+         break
+
+
+-----
+Continue
+---------> This statement will skip that particular itteration and goes to next iterations
+
+any=["pavani","mahima","sownya","indhrani"]
+for i in any:
+    if i =="sownya":
+        continue
+    print(i)
+
+pass
+----->Pass is space holder,holds the space not to get any error
+
+
+
+for i in range(1,10): # it will gives error
+     pass           #-----> it will not give error
+
+
+a=9
+b=90
+if a >=b:
+    pass
+
+number=int(input("enter number to check prime or not: "))
+count=0
+for i in range(1,number+1):
+    if number % i==0:
+        count+=1
+if count==2:
+    print(f"{number} is a prime")
+else:
+    print(f"{number} is not a prime")
+
+
+----
+Nested Loop
+----------> A loop in side the loop is called nested loop
+"""
+for j in range(2,100):
+    count=0
+    for an in range(1,j+1):
+        if j % an ==0:
+            count+=1
+    if count==2:
+        print(f"{j} is a prime")
+    else:
+        print(f"{j} is not a prime")
 
 
 
